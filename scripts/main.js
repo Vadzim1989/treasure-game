9f9498abdab6f81e3524489e500242d91f8c7794
Vadim <gomel.itstep.@gmail.com>
2021-08-12 19:49:41 +0300

    add script getdistancetotreasure

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 const hint = document.getElementById('hint');
 const map = document.querySelector('[alt="treasure_map"]');
 
-const hidTreasure = {
-    X: Math.random(),
-    Y: Math.random()
+const generateRandomNumber = (size,gap) => Math.floor(Math.random() * (size - (gap*2))) + gap;
+
+const generateTreasureCoords = ({width, heigth}, gap) => ({
+        x: generateRandomNumber(width,gap),
+        y: generateRandomNumber(heigth,gap)
+});
+
+
+function getDistanceToTreasure ({x: cX, y: cY}, {x: tX, y: tY} ) { // rename arguments
+        return Math.sqrt((cX - tX)**2 + (cY - tY)**2);
 };
 
-function hideTreasure (map) {
-    console.dir(map);
-}
-
-hideTreasure(map);
+console.log(getDistanceToTreasure({x: 2, y: 35}, {x: 51, y: 234}));
